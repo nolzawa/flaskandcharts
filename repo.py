5953248b26957bcb08567c69749d916e86fe4d2a
Sabina Safarudin <senseofeven@gmail.com>
2020-10-16 01:50:09 +0800

    Update repo.py
    Corrected typo in some functions.

--- repo.py
+++ repo.py
@@ -10,41 +10,40 @@ db = connection['TMA']
 def get_high(objDate=None):
     filter = {}
     if objDate is not None:
-        start=datetime(objDate.year, objDate.nmonth, objDate.day) #get the start of the date
+        start=datetime(objDate.year, objDate.month, objDate.day) #get the start of the date
         end=start+timedelta(days=1) # this get the ending
         filter['date'] = { #this filters only the time between start and end date will appear
             '$lt':end,
             '$gte':start
         }
-    return db.trolley.find(filter).sort("temp",-1).limit(1)
+    return db.trolley.find(filter).sort("temp",-1).limit(1) #get only the highest
 
 def get_low(objDate=None):
     filter = {}
     if objDate is not None:
-        start=datetime(objDate.year, objDate.nmonth, objDate.day) #get the start of the date
+        start=datetime(objDate.year, objDate.month, objDate.day) #get the start of the date
         end=start+timedelta(days=1) # this get the ending
         filter['date'] = { #this filters only the time between start and end date will appear
             '$lt':end,
             '$gte':start
         }
-    return db.trolley.find(filter).sort("temp",+1).limit(1)
+    return db.trolley.find(filter).sort("temp",+1).limit(1) #get only the lowest
 
 def f_date(objDate=None):
     filter = {}
     if objDate is not None:
-        start=datetime(objDate.year, objDate.nmonth, objDate.day) #get the start of the date
+        start=datetime(objDate.year, objDate.month, objDate.day) #get the start of the date
         end=start+timedelta(days=1) # this get the ending
         filter['date'] = { #this filters only the time between start and end date will appear
             '$lt':end,
             '$gte':start
         }
     return db.trolley.find(filter)
-    
 
 def count_active(objDate=None):
     filter = {}
     if objDate is not None:
-        start=datetime(objDate.year, objDate.nmonth, objDate.day) #get the start of the date
+        start=datetime(objDate.year, objDate.month, objDate.day) #get the start of the date
         end=start+timedelta(days=1) # this get the ending
         filter['date'] = { #this filters only the time between start and end date will appear
             '$lt':end,
@@ -59,22 +58,22 @@ def count_all(): #gets all unique trolley names in the collection
     r_len = len(results)
     return r_len
 
-def create_trolley(name, date, time, temp):
+def create_trolley(name, date, temp):
     assert name is not None
     assert date is not None
-    assert time is not None
     assert temp is not None
 
-    db.trolley.insert_one({'name':name, 'date': date, 'time': time, 'temp': temp})
+    DateObj = datetime.strptime(date, "%Y-%m-%d")
 
-def update_trolley(name, new_temp):
+    db.trolley.insert_one({'name':name, 'date': DateObj, 'temp': temp})
+
+def update_trolley(name, new_date):
     assert name is not None
-    assert new_temp is not None
+    assert new_date is not None
 
-    db.trolley.update_one({'name':name}, {'$set':{'temp':new_temp}})
+    db.trolley.update_one({'name':name}, {'$set':{'date':new_date}})
 
 def delete_trolley(name):
     assert name is not None
 
     db.trolley.delete_one({'name':name})
-
