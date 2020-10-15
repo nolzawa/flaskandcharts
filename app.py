dc9db0b8a941681611710a7df00bbf2c746e9993
Sabina Safarudin <senseofeven@gmail.com>
2020-10-16 01:49:05 +0800

    Update app.py
    Managed to get the x axis labels on the chart on dashboard.html. The error was in some of the repo functions where there was a typo of .nmonth instead of .month - corrected this.
    
    Issue facing is still not plotting the data into the chart's dataset

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 #app.py
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
+import json
 from datetime import datetime
 
 app = Flask(__name__)
 
 import repo
+
+@app.route("/trolley/<strdate>")
+def test_date(strdate):
+    #convert to dt obj
+    dd = datetime.strptime(strdate, '%Y-%m-%d')
+    print(type(dd))
+    print(type(strdate))
+    print(dd.year)
+    print(dd.month)
+    print(dd.day)
+    return render_template("trolley.html", dlist=repo.f_date(dd))
 
 @app.route("/trolley/delete", methods=['POST','GET'])
 def trolley_delete():
@@ -17,22 +29,20 @@ def trolley_delete():
 @app.route("/trolley/update", methods=['POST','GET'])
 def update_trolley():
     if (request.method == 'POST'):
-        repo.update_trolley(request.form.get('name'), request.form.get('temp'))
-        return "Thank you for submitting"
-    return render_template("form.html")
+        repo.update_trolley(request.form.get('name'), request.form.get('date'))
+        return "Thank you for updating"
+    return render_template("update.html")
 
 @app.route("/trolley/create", methods=['POST','GET'])
 def trolley_create():
     if (request.method == 'POST'):
-        repo.create_trolley(request.form.get('name'), request.form.get('date'),
-        request.form.get('time'), request.form.get('temp'))
+        repo.create_trolley(request.form.get('name'), request.form.get('date'), request.form.get('temp'))
         return "Thank you for submitting"
     return render_template("form.html")
 
-
 @app.route("/trolley")
 def trolley_read():
-    return render_template("trolley.html", dlist=repo.f_date(), tlist=repo.get_high())
+    return render_template("trolley.html", dlist=repo.f_date())
 
 @app.route("/temp")
 def temp_view():
@@ -44,7 +54,6 @@ def dashboard_view():
 
 #DateObj = datetime.strptime(date_string, "%Y-%m-%d") to convert into dateobj
 #to plug in the data and labels, pull out all the data into a string and plug into the chart
-
 
 if __name__ == "main":
     app.run()
